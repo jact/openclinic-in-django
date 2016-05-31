cad38702a7adbb5d7f7978e1b22ce92299086364
Jose Antonio Chavarría <jachavar@gmail.com>
2016-05-31 07:57:57 +0200

    Removed unused variable

--- medical/templatetags/setting.py
+++ medical/templatetags/setting.py
@@ -9,7 +9,7 @@ register = template.Library()
 @register.tag
 def setting(parser, token):
     try:
-        tag_name, option = token.split_contents()
+        _, option = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError, "%r tag requires a single argument" % token.contents[0]
 
