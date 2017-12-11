73244b15d65e7315fe864ca6a5ff677327567604
Jose Antonio Chavarría <jachavar@gmail.com>
2017-12-11 18:14:20 +0100

    Bumped APP_VERSION to 1.0.20171211 (#2)

--- openclinic/settings/openclinic_conf.py
+++ openclinic/settings/openclinic_conf.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2014-2015 Jose Antonio Chavarría
+# Copyright (c) 2014-2017 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,17 +14,15 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# Author: Jose Antonio Chavarría <jachavar@gmail.com>
 
 __author__ = 'Jose Antonio Chavarría'
 __license__ = 'GPLv3'
 
-# Settings for openclinic project. OpenClinic Revisited project.
+# Settings for OpenClinic project. OpenClinic Revisited project.
 
 APP_AUTHOR = __author__
 APP_NAME = 'OpenClinic'  # OpenClinic Revisited (in Django)
-APP_VERSION = '1.0.20151220'
+APP_VERSION = '1.0.20171211'
 
 CLINIC_NAME = 'My Clinic'
 CLINIC_HOURS = 'L-V 9am-3pm, S 10am-1pm'
