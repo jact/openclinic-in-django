6289ff24fd3ef01bf0ba36279a0ce7d5c11af257
Jose Antonio Chavarría <jachavar@gmail.com>
2018-08-16 11:00:22 +0200

    Fixed imports

--- medical/models/__init__.py
+++ medical/models/__init__.py
@@ -2,9 +2,9 @@
 
 # import order is important!!!
 
-from time_stamped import TimeStampedModel
-from patient import Patient
-from problem import Problem
-from test import Test
-from history import History
-from staff import Staff
+from .time_stamped import TimeStampedModel
+from .patient import Patient
+from .problem import Problem
+from .test import Test
+from .history import History
+from .staff import Staff
