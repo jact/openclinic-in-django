2787cb0014e5dd10be2d8d85b5605e7236d9c511
Jose Antonio Chavarría <jachavar@gmail.com>
2020-02-12 09:47:08 +0100

    Removed unicode support for Python2

--- medical/models/staff.py
+++ medical/models/staff.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2012-2015 Jose Antonio Chavarría
+# Copyright (c) 2012-2020 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,15 +14,12 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# Author: Jose Antonio Chavarría <jachavar@gmail.com>
 
 __author__ = 'Jose Antonio Chavarría'
 __license__ = 'GPLv3'
 
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import AbstractUser, UserManager
 
@@ -41,7 +38,6 @@ class DoctorManager(models.Manager):
         )
 
 
-@python_2_unicode_compatible
 class Staff(AbstractUser):
     STAFF_CHOICES = (
         ('A', _('Administrative')),
@@ -92,7 +88,7 @@ class Staff(AbstractUser):
             raise ValidationError(_('Collegiate number is required for doctor'))
 
     def __str__(self):
-        return '%s %s %s' % (
+        return '{} {} {}'.format(
             self.first_name,
             self.last_name,
             self.last_name_optional
