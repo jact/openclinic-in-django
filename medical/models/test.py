6a62d78f5ab5d6cd409c01a716aaaa0147267599
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-25 09:49:31 +0100

    Standardisation increased

--- medical/models/test.py
+++ medical/models/test.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2012-2020 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2012-2022 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -45,14 +45,14 @@ class Test(TimeStampedModel):
         on_delete=models.CASCADE
     )
 
-    def filename(self):
-        return os.path.basename(self.document.name)
+    class Meta:
+        app_label = 'medical'
 
     def __str__(self):
         return str(self.document)
 
-    class Meta:
-        app_label = 'medical'
+    def filename(self):
+        return os.path.basename(self.document.name)
 
 
 @receiver(pre_delete, sender=Test)
