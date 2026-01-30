22e0be5a427cbaa87e6968376603a281a3e23d15
jact <jachavar@gmail.com>
2026-01-30 20:07:56 +0100

    refactor(urls): use django.urls (re_path) for Django 5.2 compatibility

--- openclinic/urls.py
+++ openclinic/urls.py
@@ -1,9 +1,7 @@
-# -*- coding: UTF-8 -*-
-
-# Copyright (c) 2012-2018 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2012-2026 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
@@ -19,72 +17,73 @@ __author__ = 'Jose Antonio Chavarría'
 __license__ = 'GPLv3'
 
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, re_path
 from django.views.generic import TemplateView, RedirectView
 
 from ajax_select import urls as ajax_select_urls
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = [
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^grappelli/', include('grappelli.urls')),
+    re_path(r'^admin/', admin.site.urls),
 
-    url(r'^ajax_select/', include(ajax_select_urls)),
+    re_path(r'^ajax_select/', include(ajax_select_urls)),
 
-    url(
+    re_path(
         r'^$',
         RedirectView.as_view(url=reverse_lazy('welcome'), permanent=True),
         name='bootstrap'
     ),
 
-    url(
+    re_path(
         r'^info/$',
         RedirectView.as_view(url=reverse_lazy('welcome'), permanent=True),
         name='info'
     ),
 
-    url(
+    re_path(
         r'^info/welcome/$',
         TemplateView.as_view(template_name='info/welcome.html'),
         name='welcome'
     ),
 
-    url(
+    re_path(
         r'^info/readme/$',
         TemplateView.as_view(template_name='info/readme.html'),
         name='readme'
     ),
-    url(
+    re_path(
         r'^info/install/$',
         TemplateView.as_view(template_name='info/install.html'),
         name='install'
     ),
-    url(
+    re_path(
         r'^info/license/$',
         TemplateView.as_view(template_name='info/license.html'),
         name='license'
     ),
 
-    url(
+    re_path(
         r'^login/$',
         auth_views.LoginView.as_view(template_name='login.html'),
         name='openclinic_login'
     ),
-    url(
+    re_path(
         r'^logout/$',
         auth_views.LogoutView.as_view(next_page='/'),
         name='openclinic_logout'
     ),
 
-    url(r'^medical_records/', include('medical.urls')),
+    re_path(r'^medical_records/', include('medical.urls')),
 ]
 
 if settings.DEBUG:
@@ -94,7 +93,7 @@ if settings.DEBUG:
     try:
         import debug_toolbar
         urlpatterns = [
-            url(r'^__debug__/', include(debug_toolbar.urls)),
+            re_path(r'^__debug__/', include(debug_toolbar.urls)),
         ] + urlpatterns
     except ImportError:
         pass
