b37f55ef52b70f30720b6311b965f4b54a71d2a7
jact <jachavar@gmail.com>
2026-01-30 20:07:56 +0100

    fix(security): derive ALLOWED_HOSTS from environment in production settings; add production logging handlers

--- openclinic/settings/production.py
+++ openclinic/settings/production.py
@@ -1,6 +1,4 @@
-# -*- coding: UTF-8 -*-
-
-# Copyright (c) 2014-2018 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2014-2026 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,6 +23,49 @@ from .base import *
 DEBUG = False
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
-ALLOWED_HOSTS = ['*']
+# Security: Read allowed hosts from environment variable
+# Format: comma-separated list of domains, e.g., "example.com,www.example.com"
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 STATIC_ROOT = '/var/tmp/static'
+
+# Production logging configuration
+import os
+
+LOG_DIR = os.environ.get('LOG_DIR', '/var/log/openclinic')
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOGGING['handlers']['file'] = {
+    'level': 'INFO',
+    'class': 'logging.handlers.RotatingFileHandler',
+    'filename': os.path.join(LOG_DIR, 'django.log'),
+    'maxBytes': 1024 * 1024 * 10,  # 10 MB
+    'backupCount': 5,
+    'formatter': 'verbose',
+}
+
+LOGGING['handlers']['error_file'] = {
+    'level': 'ERROR',
+    'class': 'logging.handlers.RotatingFileHandler',
+    'filename': os.path.join(LOG_DIR, 'django_errors.log'),
+    'maxBytes': 1024 * 1024 * 10,  # 10 MB
+    'backupCount': 10,
+    'formatter': 'verbose',
+}
+
+LOGGING['handlers']['security_file'] = {
+    'level': 'WARNING',
+    'class': 'logging.handlers.RotatingFileHandler',
+    'filename': os.path.join(LOG_DIR, 'security.log'),
+    'maxBytes': 1024 * 1024 * 5,  # 5 MB
+    'backupCount': 10,
+    'formatter': 'verbose',
+}
+
+LOGGING['loggers']['django.security'] = {
+    'handlers': ['security_file', 'mail_admins'],
+    'level': 'WARNING',
+    'propagate': False,
+}
+
+LOGGING['loggers']['django']['handlers'] = ['file', 'mail_admins']
