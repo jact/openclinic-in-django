561ef8fe7f6fcd13450dbdaf5f3f1dcaf65027ba
jact <jachavar@gmail.com>
2026-01-30 20:09:53 +0100

    fix: Django 5 compatibility

--- medical/urls.py
+++ medical/urls.py
@@ -1,6 +1,4 @@
-# -*- coding: UTF-8 -*-
-
-# Copyright (c) 2012-2018 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2012-2026 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,9 +16,8 @@
 __author__ = 'Jose Antonio Chavarría'
 __license__ = 'GPLv3'
 
-from django.conf.urls import url
 from django.views.generic import RedirectView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, re_path
 
 from .views import (
     PatientListView, PatientSearch, PatientCreate, PatientUpdate,
@@ -33,7 +30,7 @@ from .views import (
 )
 
 urlpatterns = [
-    url(
+    re_path(
         r'^$',
         RedirectView.as_view(
             url=reverse_lazy('patient_list'), permanent=True
@@ -41,139 +38,139 @@ urlpatterns = [
         name='medical_records',
     ),
 
-    url(
+    re_path(
         r'^patient/$',
         PatientListView.as_view(),
         name='patient_list',
     ),
 
-    url(
+    re_path(
         r'^patient/search/$',
         PatientSearch.as_view(),
         name='patient_search',
     ),
 
-    url(
+    re_path(
         r'^patient/add/$',
         PatientCreate.as_view(),
         name='patient_add',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/change/$',
         PatientUpdate.as_view(),
         name='patient_change',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/delete/$',
         PatientDelete.as_view(),
         name='patient_delete',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/$',
         PatientRedirectDetail.as_view(),
         name='patient_redirect_detail',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)-(?P<slug>[-\w]+)/$',
         PatientDetail.as_view(),
         name='patient_detail',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/report/$',
         PatientMedicalReport.as_view(),
         name='patient_medical_report',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/relatives/$',
         PatientRelatives.as_view(),
         name='patient_relatives',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/tests/$',
         PatientTests.as_view(),
         name='patient_tests',
     ),
 
-    url(
+    re_path(
         r'^problem/search/$',
         ProblemSearch.as_view(),
         name='problem_search',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/problem/$',
         ProblemList.as_view(),
         name='problem_list',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/problem/add/$',
         ProblemCreate.as_view(),
         name='problem_add',
     ),
 
-    url(
+    re_path(
         r'^problem/(?P<pk>\d+)/change/$',
         ProblemUpdate.as_view(),
         name='problem_change',
     ),
 
-    url(
+    re_path(
         r'^problem/(?P<pk>\d+)/$',
         ProblemDetail.as_view(),
         name='problem_detail',
     ),
 
-    url(
+    re_path(
         r'^problem/(?P<pk>\d+)/delete/$',
         ProblemDelete.as_view(),
         name='problem_delete',
     ),
 
-    url(
+    re_path(
         r'^problem/(?P<pk>\d+)/connections/$',
         ProblemConnections.as_view(),
         name='problem_connections',
     ),
 
-    url(
+    re_path(
         r'^problem/(?P<pk>\d+)/tests/$',
         ProblemTests.as_view(),
         name='problem_tests',
     ),
 
-    url(
+    re_path(
         r'^test/(?P<pk>\d+)/delete/$',
         ProblemTestDelete.as_view(),
         name='problem_test_delete',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/history/$',
         HistoryList.as_view(),
         name='patient_history',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/history/antecedents/$',
         HistoryAntecedentsDetail.as_view(),
         name='patient_history_antecedents',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/history/antecedents/add/$',
         HistoryAntecedentsCreate.as_view(),
         name='patient_history_antecedents_add',
     ),
 
-    url(
+    re_path(
         r'^patient/(?P<pk>\d+)/history/antecedents/change/$',
         HistoryAntecedentsUpdate.as_view(),
         name='patient_history_antecedents_change',
