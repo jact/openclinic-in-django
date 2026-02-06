d28ac26b444641edd3907a4eb009bcba609b2ecd
jact <jachavar@gmail.com>
2026-02-06 18:15:05 +0100

    fix: Remove DisableMigrations, use --create-db for tests

--- openclinic/settings/test.py
+++ openclinic/settings/test.py
@@ -42,18 +42,6 @@ PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
 
-
-# Disable migrations for tests (faster)
-class DisableMigrations:
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return None
-
-
-MIGRATION_MODULES = DisableMigrations()
-
 # Silence security warnings for tests (expected in test mode)
 SILENCED_SYSTEM_CHECKS = [
     "security.W001",  # SecurityMiddleware not in MIDDLEWARE
